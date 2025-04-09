6222d4228a985ddd32c5fe71a0686f90c823931b
Han <blacksmith1710@gmail.com>
2025-04-09 23:32:14 +0630

    Order-database-input

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Dish;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -12,6 +13,35 @@ class OrderController extends Controller
     }
     public function index()
     {
-        return view('kitchen.order');
+        $dishes = Dish::orderBy('id', 'desc')->get();
+        return view('order_form', compact('dishes'));
+    }
+
+    public function submit(Request $request)
+    {
+        $data = array_filter($request->except('_token'));
+        $orderId = rand();
+        $request->table = 1;
+        foreach ($data as $key => $value) {
+            if ($value > 1) {
+                for ($i = 0; $i < $value; $i++) {
+                    $this->orderCreate($key, $request, $orderId);
+                }
+            } else {
+                $this->orderCreate($key, $request, $orderId);
+            }
+            exit();
+        }
+    }
+
+    public function orderCreate($orderId, $dish_id, $request)
+    {
+        $dish = new Dish();
+        $dish->order_id = $orderId;
+        $dish->dish_id = $dish_id;
+        $dish->table_id = $request->table;
+        $dish->status = config('orderrequest.order_status.new');
+
+        $dish->save();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
 use App\Http\Controllers\DishesController;
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes([
     'register' => false,
@@ -14,6 +12,13 @@ Auth::routes([
     'verify' => false,
 ]);
 
-Route::get('/home', [App\Http\Controllers\OrderController::class, 'index'])->name('home');
+Route::get('/', [OrderController::class, 'index'])->name('order.form');
+Route::post('order_submit', [OrderController::class, 'submit'])->name('order.submit');
 
-Route::resource('dish', DishesController::class);
+Route::resource('dish', DishesController::class)
+    ->name('edit', 'dish.edit')
+    ->name('destroy', 'dish.delete')
+    ->name('update', 'dish.update')
+    ->name('index', 'dish.index')
+    ->name('store', 'dish.store')
+    ->name('create', 'dish.create');
